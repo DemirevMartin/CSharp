0282216959eb10f67ca7f38c748b99e73d95d417
DemirevMartin <mdemirev18@gmail.com>
2021-12-08 09:53:21 +0200

    5. FormattedOutput

---
+++ 11th grade/5. FormattedOutput/BeerOrders/BeerOrders/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: User
+ * Date: 19.10.2020 г.
+ * Time: 18:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Поръчки_за_бира2._
+{
+	class Order
+    {
+        private string packaging;
+        private int year, month, day, quantity;
+        private double price;
+        
+        public Order(string pck, int y, int m, int d, int q, double p)
+        {
+            packaging=pck;
+            year=y;
+            month=m;
+            day=d;
+            quantity=q;
+            price=p;
+        }
+        public double CalcTotal()
+        {
+            return quantity*price;
+        }
+        public void Status()
+        {
+        	DateTime dt=DateTime.Today;
+            DateTime dayt=new DateTime(year, month, day);
+            if(dt>dayt)
+            {
+                Console.WriteLine("Status: unfulfilled!");
+            }
+            else
+            {
+                Console.WriteLine("Status: fulfilled!");
+            }
+        }
+        public void PrintOrder()
+        {
+            Console.WriteLine("\nInformation on ordering beer:");
+            Console.WriteLine("\n{0,9}{1,12}{2,17}{3,12}{4,19}", "Packaging", "Quantity", "Order date", "Price", "Total");
+            Console.WriteLine(new String('-', 70));
+            DateTime newdt=new DateTime(year, month, day);
+            Console.WriteLine("{0,6}{1,12}{2,21:dd MMMM yyyy}{3,13:c2}{4,18:c2}", packaging, quantity, newdt, price, CalcTotal().ToString("f2")+" лв.");
+            Console.WriteLine(new String('-', 70));
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        	string pk;
+        	int y, m, d, q;
+        	double p;
+        	Console.Write("Enter packaging: ");
+            pk=Convert.ToString(Console.ReadLine());
+            Console.Write("Enter year of the order: ");
+            y=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter month of the order: ");
+            m=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter day of the order: ");
+            d=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Requested quantity: ");
+            q=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Price: ");
+            p=Convert.ToDouble(Console.ReadLine());
+        	
+            Order beer=new Order(pk, y, m, d, q, p);
+            beer.PrintOrder();
+            beer.Status();
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ 11th grade/5. FormattedOutput/CarRent/CarRent/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+namespace CarRent
+{
+	class CarRental
+	{
+		private string model;
+		private DateTime dateTake, dateReturn;
+		private double price;
+		
+		public CarRental(string m, DateTime d1, DateTime d2, double p)
+		{
+			model=m;
+			dateTake=d1;
+			dateReturn=d2;
+			price=p;
+		}
+		int DateDiff()
+		{
+			return (dateReturn-dateTake).Days;
+		}
+		double CalcSum()
+		{
+			return DateDiff()*price;
+		}
+		double CalcDiscount()
+		{
+			double discount;
+			if(DateDiff()==2 || DateDiff()==3)
+			{
+				return discount=CalcSum()*5/100;
+			}
+			else if(DateDiff()==4 || DateDiff()==5 || DateDiff()==6)
+			{
+			    return discount=CalcSum()*10/100;	
+			}
+			else if(DateDiff()==7)
+			{
+			    return discount=CalcSum()*15/100;	
+			}
+			else
+			{
+			    return 0;
+			}
+		}
+		public void PrintInfo()
+		{
+			double total=CalcSum()-CalcDiscount();
+			Console.Write("Car rental information: {0}", model);
+			Console.WriteLine();
+			Console.WriteLine(new String('-',90));
+			Console.WriteLine("{0, 11}{1, 15}{2, 18}{3, 12}{4, 19}{5, 12}", "Rental date", "Return date", "Number of days", "Sum", "Discount", "Total");
+			Console.WriteLine("{0, 11:dd MMMM yyyy}{1, 15:dd MMMM yyyy}{2, 11}{3, 23:c2}{4, 16:c2}{5, 14:c2}", dateTake, dateReturn, DateDiff(), CalcSum(), CalcDiscount(),  total);
+			Console.WriteLine(new String('-',90));
+		}
+	}
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			CarRental car=new CarRental("Mazda 6", new DateTime(2020, 5, 3), new DateTime(2020, 5, 8), 35);
+			car.PrintInfo();
+			
+			Console.ReadKey(true);
+		}
+	}
+}
