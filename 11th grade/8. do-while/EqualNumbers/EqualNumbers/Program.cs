c09cefc61c777fd00b29c128a3868b0292fdd8f6
DemirevMartin <mdemirev18@gmail.com>
2021-12-10 23:15:00 +0200

    do-while loop

---
+++ 11th grade/8. do-while/EqualNumbers/EqualNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace EqualNumbers
+{
+class EqNumbers
+{
+    int a, b, num;
+    public EqNumbers(int _a)
+    {
+        a = _a;
+    }
+
+    int CountEqNumbers()
+    {
+       int c = 0, m = 1;
+       b = a;
+       do
+       {
+          if(a == b) { c++; num = b;}
+          else
+          {
+              if(m < c) { m = c; }
+              c = 1; b = a;
+          }
+      Console.Write("Въведете цяло число (0 за край): ");
+      a = int.Parse(Console.ReadLine());
+      }
+     while(a != 0);
+     Console.WriteLine(new String('-', 40));
+     if(m < c) { m = c; }
+     return m;
+	}
+	public void Print()
+	{
+		Console.WriteLine("Брой повторения: " + CountEqNumbers());
+		Console.WriteLine("Последователно повтарящо се число: " + num);
+		Console.WriteLine(new String('-', 40));
+	}
+}
+class Program
+{
+public static void Main(string[] args)
+{
+     Console.Write("Въведете цяло число (0 за край): ");
+     int a = int.Parse(Console.ReadLine());
+     EqNumbers en = new EqNumbers(a);
+     en.Print();
+     Console.Write(". . . ");
+     Console.ReadKey(true);
+}
+}
+}
---
+++ 11th grade/8. do-while/Money/Money/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: zlati
+ * Date: 4.11.2020 г.
+ * Time: 08:25 ч.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Money
+{
+	class Banknotes
+	{
+		int total, billsOf5, billsOf10, billsOf20;
+		public Banknotes()
+		{
+			SortAndCount();
+		}
+		private void SortAndCount()
+		{
+			int a, sum = 0;
+			do
+			{
+				Console.Write("Въведи стойността на поредната банкнота - 5, 10, 20 или 0 за край: ");
+				a = int.Parse(Console.ReadLine());
+				sum+=a;
+				if(a==5)
+				{
+					billsOf5++;
+					total++;
+				}
+				else if(a==10)
+				{
+					billsOf10++;
+					total++;
+				}
+				else if(a==20)
+				{
+					billsOf20++;
+					total++;
+				}
+			}
+			while (a!=0);
+			
+		}
+		public void Print()
+	    {
+		    Console.WriteLine("Справка за наличността");	
+		    Console.WriteLine("Банкноти от 5 лв.: {0} броя", billsOf5);
+		    Console.WriteLine("Банкноти от 10 лв.: {0} броя", billsOf10);
+		    Console.WriteLine("Банкноти от 20 лв.: {0} броя", billsOf20);
+		    Console.WriteLine("Общо: {0} банкноти на стойност {1} лв.", total, billsOf5*5 + billsOf10*10 + billsOf20*20);
+	    }
+	}
+	
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Banknotes bank=new Banknotes();
+			bank.Print();
+			
+			Console.ReadKey(true);
+		}
+	}
+}
